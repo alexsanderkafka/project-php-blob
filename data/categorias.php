dbf14a3248a204c4e8384dd309442139885281ed
alexsander kafka <alexsanderkafka2001alex@gmail.com>
2023-10-03 11:14:16 -0300

    Preparando os dados mockados

---
+++ data/categorias.php
@@ -0,0 +1,33 @@
+<?php
+
+    $posts = [
+        [
+            'id' => 1,
+            'title' => 'Programando em PHP',
+            'description' => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+            'tags' => ['programação', 'PHP', 'lógica de programação'],
+            'img' => 'prog-1.jpg';
+        ],
+        [
+            'id' => 2,
+            'title' => 'Orientação a Objeto com Java',
+            'description' => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+            'tags' => ['Java', 'OOP', 'Programação Avançada'],
+            'img' => 'prog-2.jpg';
+        ],
+        [
+            'id' => 3,
+            'title' => 'A importância da lógica de programação',
+            'description' => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+            'tags' => ['lógica', 'programação', 'algoritmo'],
+            'img' => 'prog-3.jpg';
+        ],
+        [
+            'id' => 4,
+            'title' => 'HTML',
+            'description' => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+            'tags' => ['HTML', 'PHP', 'CSS'],
+            'img' => 'prog-4.jpg';
+        ]
+    ]
+?>  
