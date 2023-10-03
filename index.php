e219f3a11c92e48b7c112b8fb7911791cf82d579
alexsander kafka <alexsanderkafka2001alex@gmail.com>
2023-10-03 11:29:16 -0300

    Fazendo algumas configurações

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+    include_once("helpers/url.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog</title>
+
+    <!--ESTILOS DO PROJECT-->
+    <link rel="stylesheet" href="<?= $BASE_URL ?>/css/reset.css">
+    <link rel="stylesheet" href="<?= $BASE_URL ?>/css/style.css">
+
+    <!-- GOOGLE FONTS -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;700&display=swap" rel="stylesheet">
+</head>
+<body>
+    <h1>Hello world!</h1>
+    <header>
+        <nav>
+
+        </nav>
+    </header>
+    
+</body>
+</html>
